71ded53435642ccbf62d2ddf07f78951cb6c2d0e
Hajime Hoshi <hajimehoshi@gmail.com>
2025-04-20 23:19:01 +0900

    basicwidget: bug fix: add util.go

---
+++ basicwidget/util.go
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 Hajime Hoshi
+
+package basicwidget
+
+import "slices"
+
+func adjustSliceSize[T any](slice []T, size int) []T {
+	if len(slice) == size {
+		return slice
+	}
+	if len(slice) < size {
+		return slices.Grow(slice, size-len(slice))[:size]
+	}
+	return slice[:size]
+}
