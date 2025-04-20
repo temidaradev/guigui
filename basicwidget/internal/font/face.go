625670c41aa6f9cb30711b6ee89ef9f7ad2bbc93
Hajime Hoshi <hajimehoshi@gmail.com>
2025-04-20 17:10:28 +0900

    basicwidget/internal/font: FaceSources should return approprite ordered faces
    Closes #68

--- basicwidget/internal/font/face.go
+++ basicwidget/internal/font/face.go
@@ -4,6 +4,7 @@
 package font
 
 import (
+	"cmp"
 	"slices"
 	"strings"
 
@@ -41,6 +42,63 @@ func (f *FaceChooser) Register(faceSource *text.GoTextFaceSource, priority func(
 	})
 }
 
+func (f *FaceChooser) faceSources(size float64, weight text.Weight, locales []language.Tag) []*text.GoTextFaceSource {
+	type priority struct {
+		priority          float64
+		localeIndex       int
+		registrationIndex int
+	}
+
+	priorities := map[*text.GoTextFaceSource]priority{}
+
+	for i, l := range locales {
+		for j, fp := range f.priorityFuncs {
+			p := min(max(fp.priority(FaceSourceHint{
+				Size:   size,
+				Weight: weight,
+				Locale: l,
+			}), 0), 1)
+			if priorities[fp.faceSource].priority >= p {
+				continue
+			}
+			priorities[fp.faceSource] = priority{
+				priority:          p,
+				localeIndex:       i,
+				registrationIndex: j,
+			}
+		}
+	}
+
+	var faceSources []*text.GoTextFaceSource
+	for _, fp := range f.priorityFuncs {
+		faceSources = append(faceSources, fp.faceSource)
+	}
+
+	slices.SortStableFunc(faceSources, func(a, b *text.GoTextFaceSource) int {
+		ap, aOk := priorities[a]
+		bp, bOk := priorities[b]
+		if !aOk && !bOk {
+			return 0
+		}
+		if !aOk {
+			return 1
+		}
+		if !bOk {
+			return -1
+		}
+		if ap.priority != bp.priority {
+			return -cmp.Compare(ap.priority, bp.priority)
+		}
+		if ap.localeIndex != bp.localeIndex {
+			return cmp.Compare(ap.localeIndex, bp.localeIndex)
+		}
+		// Prefer later registrations.
+		return -cmp.Compare(ap.registrationIndex, bp.registrationIndex)
+	})
+
+	return faceSources
+}
+
 func (f *FaceChooser) Face(size float64, weight text.Weight, ligature bool, locales []language.Tag) text.Face {
 	var localeStrs []string
 	for _, l := range locales {
@@ -57,31 +115,7 @@ func (f *FaceChooser) Face(size float64, weight text.Weight, ligature bool, loca
 		return f
 	}
 
-	fps := append([]faceSourceWithPriorityFunc{}, f.priorityFuncs...)
-
-	var faceSources []*text.GoTextFaceSource
-	for _, l := range locales {
-		var highestPriority float64
-		var index int
-		for i, fp := range fps {
-			p := min(max(fp.priority(FaceSourceHint{
-				Size:   size,
-				Weight: weight,
-				Locale: l,
-			}), 0), 1)
-			// If the priority is the same, the later one is used.
-			if highestPriority <= p {
-				highestPriority = p
-				index = i
-			}
-		}
-		// TODO: Now only one face is added for each locale. Add more faces (#68).
-		faceSources = append(faceSources, fps[index].faceSource)
-		fps = slices.Delete(fps, index, index+1)
-		if len(fps) == 0 {
-			break
-		}
-	}
+	faceSources := f.faceSources(size, weight, locales)
 
 	var fs []text.Face
 	var lang language.Tag
---
+++ basicwidget/internal/font/face_test.go
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 Hajime Hoshi
+
+package font_test
+
+import (
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	"github.com/hajimehoshi/guigui/basicwidget/internal/font"
+	"golang.org/x/text/language"
+)
+
+func TestFaceSources(t *testing.T) {
+	faceSources := map[string]*text.GoTextFaceSource{
+		"en":        {},
+		"fr":        {},
+		"ja":        {},
+		"zh-Hans":   {},
+		"zh-Hant":   {},
+		"fallback1": {},
+		"fallback2": {},
+	}
+
+	var fc font.FaceChooser
+	fc.Register(faceSources["en"], func(hint font.FaceSourceHint) float64 {
+		if hint.Locale == language.English {
+			return 1
+		}
+		return 0
+	})
+	fc.Register(faceSources["fr"], func(hint font.FaceSourceHint) float64 {
+		if hint.Locale == language.French {
+			return 1
+		}
+		return 0
+	})
+	fc.Register(faceSources["ja"], func(hint font.FaceSourceHint) float64 {
+		if hint.Locale == language.Japanese {
+			return 1
+		}
+		return 0
+	})
+	fc.Register(faceSources["zh-Hans"], func(hint font.FaceSourceHint) float64 {
+		if hint.Locale == language.SimplifiedChinese {
+			return 1
+		}
+		if hint.Locale == language.Chinese {
+			return 2.0 / 3.0
+		}
+		if hint.Locale == language.TraditionalChinese {
+			return 1.0 / 3.0
+		}
+		return 0
+	})
+	fc.Register(faceSources["zh-Hant"], func(hint font.FaceSourceHint) float64 {
+		if hint.Locale == language.TraditionalChinese {
+			return 1
+		}
+		if hint.Locale == language.Chinese {
+			return 2.0 / 3.0
+		}
+		if hint.Locale == language.SimplifiedChinese {
+			return 1.0 / 3.0
+		}
+		return 0
+	})
+	fc.Register(faceSources["fallback1"], func(hint font.FaceSourceHint) float64 {
+		return 0.1
+	})
+	// If the priority is the same, the later registered face source is preferred.
+	fc.Register(faceSources["fallback2"], func(hint font.FaceSourceHint) float64 {
+		return 0.1
+	})
+
+	testCases := []struct {
+		Locales []language.Tag
+		Out     []string
+	}{
+		{
+			Locales: nil,
+			Out:     []string{"en", "fr", "ja", "zh-Hans", "zh-Hant", "fallback1", "fallback2"},
+		},
+		{
+			Locales: []language.Tag{
+				language.English,
+			},
+			Out: []string{"en", "fallback2", "fallback1", "fr", "ja", "zh-Hans", "zh-Hant"},
+		},
+		{
+			Locales: []language.Tag{
+				language.SimplifiedChinese,
+			},
+			Out: []string{"zh-Hans", "zh-Hant", "fallback2", "fallback1", "en", "fr", "ja"},
+		},
+		{
+			Locales: []language.Tag{
+				language.SimplifiedChinese,
+				language.Japanese,
+			},
+			Out: []string{"zh-Hans", "ja", "zh-Hant", "fallback2", "fallback1", "en", "fr"},
+		},
+		{
+			Locales: []language.Tag{
+				language.English,
+				language.French,
+				language.Japanese,
+				language.SimplifiedChinese,
+				language.TraditionalChinese},
+			Out: []string{"en", "fr", "ja", "zh-Hans", "zh-Hant", "fallback2", "fallback1"},
+		},
+		{
+			Locales: []language.Tag{
+				language.TraditionalChinese,
+				language.SimplifiedChinese,
+				language.English,
+				language.Japanese,
+				language.French},
+			Out: []string{"zh-Hant", "zh-Hans", "en", "ja", "fr", "fallback2", "fallback1"},
+		},
+	}
+	for _, tc := range testCases {
+		var localeStrs []string
+		for _, l := range tc.Locales {
+			localeStrs = append(localeStrs, l.String())
+		}
+		t.Run(strings.Join(localeStrs, ","), func(t *testing.T) {
+			var got []string
+			for _, fs := range fc.FaceSources(1, text.WeightNormal, tc.Locales) {
+				for name, fs2 := range faceSources {
+					if fs == fs2 {
+						got = append(got, name)
+						break
+					}
+				}
+			}
+			want := tc.Out
+			if !slices.Equal(got, want) {
+				t.Errorf("got %v, want %v", got, want)
+			}
+		})
+	}
+
+}
