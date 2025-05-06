b3a4510ff3d7b779c4bdc62b315e5a665492891e
Temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-05-07 01:49:38 +0300

    linux system-theme detection

---
+++ internal/colormode/colormode_linux.go
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 The Guigui Authors
+
+package colormode
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func systemColorMode() ColorMode {
+	mode := checkGSettings()
+	if mode != Unknown {
+		return mode
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return Light
+	}
+	mode = checkGtkSettingsFile(homeDir + "/.config/gtk-4.0/settings.ini")
+	if mode != Unknown {
+		return mode
+	}
+
+	mode = checkGtkSettingsFile(homeDir + "/.config/gtk-3.0/settings.ini")
+	if mode != Unknown {
+		return mode
+	}
+
+	return Light
+}
+
+func checkGSettings() ColorMode {
+	out, err := exec.Command("gsettings", "get", "org.gnome.desktop.interface", "color-scheme").Output()
+	if err != nil {
+		return Unknown
+	}
+
+	value := strings.TrimSpace(string(out))
+	value = strings.Trim(value, "'")
+
+	switch value {
+	case "prefer-dark":
+		return Dark
+	case "default", "prefer-light":
+		return Light
+	default:
+		return Unknown
+	}
+}
+
+func checkGtkSettingsFile(path string) ColorMode {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Unknown
+	}
+
+	content := strings.ToLower(string(data))
+	if strings.Contains(content, "gtk-application-prefer-dark-theme=true") {
+		return Dark
+	}
+	return Light
+}
--- internal/colormode/colormode_others.go
+++ internal/colormode/colormode_others.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2025 The Guigui Authors
 
-//go:build !darwin && !js && !windows
+//go:build !darwin && !js && !linux && !windows
 
 package colormode
 
