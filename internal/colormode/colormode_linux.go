423e2ef6874032ad92262c52c1d040499e4edc54
Temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-05-08 19:45:41 +0300

    internal/colormode: add Linux system theme detection (#112)
    Updates #79

---
+++ internal/colormode/colormode_linux.go
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 The Guigui Authors
+
+package colormode
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func systemColorMode() ColorMode {
+	if mode := checkGSettings(); mode != Unknown {
+		return mode
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return Unknown
+	}
+	if mode := checkGTKSettingsFile(filepath.Join(homeDir+".config", "gtk-4.0", "settings.ini")); mode != Unknown {
+		return mode
+	}
+
+	if mode := checkGTKSettingsFile(filepath.Join(homeDir+".config", "gtk-3.0", "settings.ini")); mode != Unknown {
+		return mode
+	}
+
+	return Unknown
+}
+
+func checkGSettings() ColorMode {
+	out, err := exec.Command("gsettings", "get", "org.gnome.desktop.interface", "color-scheme").Output()
+	if err != nil {
+		return Unknown
+	}
+
+	value := strings.TrimSpace(string(out))
+	value = strings.Trim(value, "'")
+
+	switch value {
+	case "prefer-dark":
+		return Dark
+	case "default", "prefer-light":
+		return Light
+	default:
+		return Unknown
+	}
+}
+
+func checkGTKSettingsFile(path string) ColorMode {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Unknown
+	}
+
+	content := strings.ToLower(string(data))
+	if strings.Contains(content, "gtk-application-prefer-dark-theme=true") {
+		return Dark
+	}
+	return Light
+}
